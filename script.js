b049da796106fd7445294b2acc446a2420aeef7a
Ravendra Payasi <payasi.ravendra@gmail.com>
2025-12-23 15:55:36 +0530

    changes has been added

--- script.js
+++ script.js
@@ -1,60 +1,64 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxFG2fl43rAlhx-l2Mt39L7GzSawzELK1JHbdJsrNu456MGY7I9Xy83qfqcXr2bqCP3/exec";
+const SHEET_URL =
+  "https://script.google.com/macros/s/AKfycbxFG2fl43rAlhx-l2Mt39L7GzSawzELK1JHbdJsrNu456MGY7I9Xy83qfqcXr2bqCP3/exec";
 
-fetch(API_URL)
+fetch(SHEET_URL)
   .then(res => res.json())
-  .then(data => {
-    const nodes = data.map(item => ({
+  .then(data => buildTree(data));
+
+function buildTree(data) {
+  const map = {};
+  data.forEach(item => {
+    map[item.id] = {
       id: item.id,
+      parent: item.parent,
       name: item.name,
-      parent: item.parent_id || null,
       role: item.role,
-      color: item.color
-    }));
-    
-    makeOrgChart(nodes);
-  });
-
-function makeOrgChart(nodes) {
-  const datamap = {};
-
-  nodes.forEach(n => {
-    datamap[n.id] = {
-      id: n.id,
-      name: n.name,
-      title: n.role,
-      color: n.color
+      children: []
     };
   });
 
-  nodes.forEach(n => {
-    if (n.parent) {
-      if (!datamap[n.parent].children) {
-        datamap[n.parent].children = [];
-      }
-      datamap[n.parent].children.push(datamap[n.id]);
+  let root = null;
+  data.forEach(item => {
+    if (item.parent) {
+      map[item.parent].children.push(map[item.id]);
+    } else {
+      root = map[item.id];
     }
   });
 
-  const rootNodes = nodes.filter(n => !n.parent).map(n => datamap[n.id]);
-
-  $("#chart-container").orgchart({
-    'data': rootNodes[0],
-    'nodeContent': 'title',
-    'pan': true,
-    'zoom': true,
-    'createNode': function($node, data) {
-      $node.css("border-color", colorMap(data.color));
-      $node.find(".title").css("background-color", colorMap(data.color));
-    }
-  });
-}
-
-function colorMap(color) {
-  switch(color) {
-    case 'yellow': return '#f5c400';
-    case 'green':  return '#59d98e';
-    case 'blue':   return '#4db5ff';
-    case 'purple': return '#b36bff';
-    default:       return '#888';
+  function createNode(person) {
+    return {
+      innerHTML: `
+        <div class="node">
+          <img src="https://cdn-icons-png.flaticon.com/512/847/847969.png"/>
+          <div class="name">${person.name}</div>
+          <div class="role">${person.role || ""}</div>
+        </div>
+      `,
+      collapsable: true,
+      children: person.children.map(createNode)
+    };
   }
+
+  new Treant({
+    chart: {
+      container: "#tree-simple",
+      rootOrientation: "NORTH",
+      nodeAlign: "CENTER",
+      levelSeparation: 80,
+      siblingSeparation: 40,
+      subTeeSeparation: 60,
+      connectors: {
+        type: "curve",
+        style: {
+          stroke: "#8b5cf6",
+          "stroke-width": 4
+        }
+      },
+      node: {
+        collapsable: true
+      }
+    },
+    nodeStructure: createNode(root)
+  });
 }
